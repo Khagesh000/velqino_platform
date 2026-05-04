0fa405938b346b570ae7c848bebbd0cc92eeefe8
Yeragana Khageshwar rao <yeraganakhagesh@gmail.com>
2026-05-04 16:34:08 +0530

    add vercel url to cors

--- server/velqino_backend/velqino_backend/settings.py
+++ server/velqino_backend/velqino_backend/settings.py
@@ -22,6 +22,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "https://velqino-backend.onrender.com",
+    "https://velqino-platform.vercel.app",
 ]
 
 AUTH_USER_MODEL = 'identity.User'
