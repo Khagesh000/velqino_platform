ea3be4448189766c5b7e587ea1fb4c9397c9a931
Yeragana Khageshwar rao <yeraganakhagesh@gmail.com>
2026-05-07 12:11:52 +0530

    fix page.js dynamic import with use client

--- velqino_frontend/src/app/page.js
+++ velqino_frontend/src/app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import PageWrapper from '@/features/common/PageWrapper';
 import dynamic from 'next/dynamic';
 
@@ -5,16 +6,6 @@ const HomePage = dynamic(() => import('@/features/common/HomePage/HomePage'), {
   ssr: false,
   loading: () => null
 });
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-};
-
-export const metadata = {
-  title: "Home - Velqino Platform",
-  description: "Find the best wholesalers and retailers near you",
-};
 
 export default function Home() {
   return (
